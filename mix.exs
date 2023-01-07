ea21a1ab6ed40eadee4d0321f585937c8f33119b
Tom Taylor <tom@tomtaylor.co.uk>
2023-01-07 16:46:39 +0000

    Add mix.exs

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule TreeWalker.MixProject do
   def project do
     [
       app: :tree_walker,
+      description: description(),
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -15,6 +17,17 @@ defmodule TreeWalker.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp description do
+    "Recursively walks a directory tree, returning the files discovered in a Stream"
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/breakroom/tree_walker"}
     ]
   end
 
