381d1e58d382b7661931bc739738d3b9d887187f
Tom Taylor <tom@tomtaylor.co.uk>
2023-01-09 16:47:42 +0000

    Bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TreeWalker.MixProject do
     [
       app: :tree_walker,
       description: description(),
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       package: package(),
